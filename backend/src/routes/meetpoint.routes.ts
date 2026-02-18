88e02cb445fb003dbbb7ecabaf5891313a46d83f
Chinmaystar <chinmaysabharwal123@gmail.com>
2026-02-18 19:42:14 +0530

    partly working meetview

--- backend/src/routes/meetpoint.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { calculateMeetPoint } from "../controller/meetpoint.controller";
-
-const router = Router();
-
-// POST /api/meeting-point
-router.post("/", calculateMeetPoint);
-
-export default router;
